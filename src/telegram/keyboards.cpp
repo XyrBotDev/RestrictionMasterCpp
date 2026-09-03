16ff5854cbf44f74728d79496e9ac9a485c9c5b2
XyrBotDev <py658112@gmail.com>
2026-09-03 14:27:37 +0530

    Create keyboards.cpp

---
+++ src/telegram/keyboards.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace Keyboards {
+
+std::string mainMenu();
+std::string premiumMenu();
+std::string botList();
+std::string botControl(int botNumber);
+std::string maintenanceMenu(int botNumber);
+std::string rangeDownloadControls();
+
+}
