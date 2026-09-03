a68c7a252573c28bfedd0484c9b813c150cd8307
XyrBotDev <py658112@gmail.com>
2026-09-03 14:11:45 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include "../include/config.hpp"
 
+int main() {
+    Config config;
+
+    std::cout << "RestrictionMasterCpp started successfully!"
+              << std::endl;
+
+    return 0;
+}
