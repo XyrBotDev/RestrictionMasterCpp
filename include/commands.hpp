9836983bb41aac2c11613adda2885c40dae901e9
XyrBotDev <py658112@gmail.com>
2026-09-03 14:35:44 +0530

    Create commands.hpp

---
+++ include/commands.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+namespace Commands {
+
+std::string start();
+std::string premium();
+std::string addBot();
+std::string botList();
+std::string botStats();
+std::string botMin();
+std::string botCast();
+
+}
