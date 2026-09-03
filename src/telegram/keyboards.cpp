6409c13940c7cd08341314e07ce6f8e233082d34
XyrBotDev <py658112@gmail.com>
2026-09-03 14:31:51 +0530

    Update keyboards.cpp

--- src/telegram/keyboards.cpp
+++ src/telegram/keyboards.cpp
@@ -1,14 +1,29 @@
-#pragma once
-
-#include <string>
+#include "../../include/keyboards.hpp"
 
 namespace Keyboards {
 
-std::string mainMenu();
-std::string premiumMenu();
-std::string botList();
-std::string botControl(int botNumber);
-std::string maintenanceMenu(int botNumber);
-std::string rangeDownloadControls();
+std::string mainMenu() {
+    return "MAIN_MENU";
+}
+
+std::string premiumMenu() {
+    return "PREMIUM_MENU";
+}
+
+std::string botList() {
+    return "BOT_LIST";
+}
+
+std::string botControl(int botNumber) {
+    return "BOT_CONTROL_" + std::to_string(botNumber);
+}
+
+std::string maintenanceMenu(int botNumber) {
+    return "MAINTENANCE_" + std::to_string(botNumber);
+}
+
+std::string rangeDownloadControls() {
+    return "RANGE_DOWNLOAD_CONTROLS";
+}
 
 }
