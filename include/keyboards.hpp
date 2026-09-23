97705c11a67c11150bcb9639793450631b4ea873
Xyr <py658112@gmail.com>
2026-09-23 16:13:43 +0530

    Refactor function declarations in keyboards.hpp

--- include/keyboards.hpp
+++ include/keyboards.hpp
@@ -5,10 +5,23 @@
 namespace Keyboards {
 
 std::string mainMenu();
+
 std::string premiumMenu();
+
+std::string settingsMenu();
+
 std::string botList();
-std::string botControl(int botNumber);
-std::string maintenanceMenu(int botNumber);
+
+std::string botControl(
+    int botNumber
+);
+
+std::string maintenanceMenu(
+    int botNumber
+);
+
+std::string dummyRefresh();
+
 std::string rangeDownloadControls();
 
 }
