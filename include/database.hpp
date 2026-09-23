24cbb12acfa393a3067a63ce591cfd7a0ab65a51
Xyr <py658112@gmail.com>
2026-09-23 19:25:02 +0530

    Refactor includes and update Database class structure

--- include/database.hpp
+++ include/database.hpp
@@ -1,26 +1,33 @@
 #pragma once
 
-#include "../include/user.hpp"
-#include "../include/bot.hpp"
+#include "user.hpp"
 
 #include <cstdint>
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
 
+namespace mongocxx {
+class instance;
+class client;
+}
+
 class Database {
 public:
-    Database(
-        const std::string& mongoUri,
-        const std::string& databaseName
-    );
+    Database(const std::string& mongoUri,
+             const std::string& databaseName);
+
+    ~Database();
+
+    Database(const Database&) = delete;
+    Database& operator=(const Database&) = delete;
 
     bool connect();
     bool isConnected() const;
 
     bool saveUser(const User& user);
     std::optional<User> getUser(std::int64_t userId);
-
     bool updateUser(const User& user);
     bool deleteUser(std::int64_t userId);
 
@@ -32,5 +39,9 @@ public:
 private:
     std::string mongoUri_;
     std::string databaseName_;
+
     bool connected_ = false;
+
+    std::unique_ptr<mongocxx::instance> instance_;
+    std::unique_ptr<mongocxx::client> client_;
 };
